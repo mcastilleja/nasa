29a3366bf5be7200a8f3eb83720f46f26a7a1172
Miguel Castilleja <castilleja.miguel@gmail.com>
2022-10-04 14:06:43 -0500

    update js

--- src/server.js
+++ src/server.js
@@ -22,27 +22,3 @@ function inicio (req, res){
 function ruta ( url ){
     return path.resolve(`${__dirname}${url}`)
 }
-
-
-/*const API_KEY = "RGMnpkN48HcdryavwwP25OxOZaNqzhT3bAwDv8Pq";
-const URL_APOD = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
-
-const getData = async (url) => {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-
-    return data;
-  } catch (error) {
-    console.error(error);
-    return error;
-  }
-};
-
-const showData = async () => {
-  const finalData = await getData(URL_APOD);
-  console.log(finalData);
-  return finalData;
-};
-
-showData();*/
