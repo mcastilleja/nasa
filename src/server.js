daa7782a66c2e649346d9a3eeb4d96ee0a425d79
Miguel Castilleja <castilleja.miguel@gmail.com>
2022-10-04 14:42:45 -0500

    Sección media

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ const port = 8080;
 app.get("/", inicio);
 app.use(express.static('css'));
 app.use(express.static('src'));
+app.use(express.static('media'));
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
