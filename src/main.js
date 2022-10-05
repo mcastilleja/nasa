ab95f1782528b00f0401127b377efc0c156746bf
Miguel Castilleja <castilleja.miguel@gmail.com>
2022-10-05 15:58:24 -0500

    Update JavaScripts

--- src/main.js
+++ src/main.js
@@ -3,38 +3,90 @@ const URL_APOD = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=`;
 
 let today = new Date();
 let actualD = parseInt(today.getDate());
-let actualM = parseInt(today.getMonth() + 1); 
+let actualM = parseInt(today.getMonth() + 1);
 let actualY = parseInt(today.getFullYear());
 
+const select = document.getElementById("cards");
 
 const getData = async (url) => {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    if( data.code != 400 ){
-        return data;
-    }
+    return data;
   } catch (error) {
     console.error(error);
     return error;
   }
 };
 
+const html = (json) => {
+  let container = document.createElement("div");
+  container.setAttribute("id", json.date);
+  container.classList.add("container");
+
+  let imgCont = document.createElement("div");
+  imgCont.classList.add("img");
+
+  if (json.url.substring(0, 23) === "https://www.youtube.com") {
+    let img = document.createElement("iframe");
+    img.setAttribute("src", json.url);
+    img.setAttribute("title", json.title);
+    img.setAttribute("frameborder", 0);
+    img.setAttribute("allow", "autoplay; encrypted-media");
+    //img.setAttribute("allowfullscreen");
+
+    imgCont.appendChild(img);
+  } else {
+    let img = document.createElement("img");
+    img.setAttribute("src", json.url);
+    img.setAttribute("alt", json.title);
+
+    imgCont.appendChild(img);
+  }
+
+  let title = document.createElement("div");
+  title.classList.add("title");
+
+  let h2 = document.createElement("h2");
+  h2.innerHTML = json.title;
+
+  title.appendChild(h2);
+
+  let description = document.createElement("div");
+  description.classList.add("description");
+
+  let p = document.createElement("p");
+  p.innerHTML = json.explanation.substring(0, 210);
+
+  description.appendChild(p);
+
+  container.append(imgCont, title, description);
+
+  select.append(container);
+};
+
 const showData = async () => {
-
-    for(let i = 1996; i <= actualY; i++){
-        for(let j = 1; j <= 12; j++) {
-            for(let k = 1; k <= 30; k++ ){
-
-                const finalData = await getData(`${URL_APOD}${i}-${j}-${k}`);
-    
-                console.log(finalData);
-
-            }
+  for (let i = 2022; i <= actualY; i++) {
+    for (let j = 9; j <= actualM; j++) {
+      if (j === actualM) {
+        for (let k = 1; k <= actualD; k++) {
+          const finalData = await getData(`${URL_APOD}${i}-${j}-${k}`);
+          if (finalData.code != 400) {
+            html(finalData);
+          }
         }
+      } else {
+        for (let k = 1; k <= 30; k++) {
+          const finalData = await getData(`${URL_APOD}${i}-${j}-${k}`);
+          if (finalData.code != 400) {
+            html(finalData);
+          }
+        }
+      }
     }
-    return finalData;
+  }
 
+  return finalData;
 };
 
 showData();
--- src/server.js
+++ src/server.js
@@ -4,22 +4,34 @@ const express = require("express");
 const app = express();
 const path = require('path');
 const home = "/../public/index.html"
+const cat = "/../public/Categoria.html"
 
 const port = 8080;
 
+// Rutas pagina
 app.get("/", inicio);
+app.get("/categoria", categoria);
+
+// Carpetas en el proyecto
 app.use(express.static('css'));
 app.use(express.static('src'));
 app.use(express.static('media'));
 
+// Puerto del express
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
 
+// Funciones de las rutas
 function inicio (req, res){
     res.sendFile(ruta(home));
 }
 
+function categoria (req, res){
+    res.sendFile(ruta(cat));
+}
+
+// Marcador de rutas
 function ruta ( url ){
     return path.resolve(`${__dirname}${url}`)
 }
