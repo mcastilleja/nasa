097f28cb181e7d01cf6f8915eecf42fbcaf13030
Miguel Castilleja <castilleja.miguel@gmail.com>
2022-10-08 16:31:05 -0500

    Cambios en estructura

---
+++ server.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const port = 8080;
+const ruta = `${__dirname}/public/`
+
+// Rutas pagina
+app.get("/", (req, res) => {
+  res.sendFile( ruta + "index.html");
+});
+app.get("/categoria",(req, res) => {
+  res.sendFile( ruta + "Categoria.html");
+});
+app.get("/date=:id", (req, res) => {
+  res.sendFile( ruta + "id.html");
+});
+
+// Carpetas en el proyecto
+app.use(express.static( ruta + "css"));
+app.use(express.static( ruta + "src"));
+app.use(express.static( ruta + "media"));
+
+// Puerto del express
+app.listen(port);
